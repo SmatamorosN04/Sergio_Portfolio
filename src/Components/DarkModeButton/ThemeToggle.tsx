e07f4400ac5ebeda099ae35289b6cc4a575857c0
Sergio Matamoros <124097535+SmatamorosN04@users.noreply.github.com>
2026-01-19 18:37:13 -0600

    darktheme saved with localstorage

--- src/Components/DarkModeButton/ThemeToggle.tsx
+++ src/Components/DarkModeButton/ThemeToggle.tsx
@@ -2,13 +2,20 @@ import {useEffect, useState} from "react";
 import * as React from "react";
 
 const ThemeToggle: React.FC = () => {
-    const [darkMode, setDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(() => {
+        const saved = localStorage.getItem('theme');
+        return saved === 'dark';
+
+    });
+
 
     useEffect(() => {
         if(darkMode){
             document.documentElement.classList.add('dark');
+            localStorage.setItem('theme','dark');
         } else {
             document.documentElement.classList.remove('dark');
+            localStorage.setItem('theme', 'light');
         }
     }, [darkMode]);
 
