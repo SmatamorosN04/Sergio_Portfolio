72b0a3edb25cc42a1556a6edf4356088eb77eda2
Sergio Matamoros <smatamorosn12@gmail.com>
2026-06-30 16:18:56 -0600

    Last change

--- desktop-portfolio/src/Components/Taskbar.tsx
+++ desktop-portfolio/src/Components/Taskbar.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useEffect, useState } from "react";
 import * as Icons from "lucide-react";
 import type { WindowApp } from "../Pages/Desktop";
